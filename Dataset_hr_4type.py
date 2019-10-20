51ac94515aeb5dfbc202e492d61ccf1cd8afac88
david li <2055429740@qq.com>
2019-10-20 15:57:38 +0800

    Add files via upload

---
+++ Dataset_hr_4type.py
@@ -0,0 +1,259 @@
+import scipy.sparse as sp
+import numpy as np
+import random
+class Dataset(object):
+   
+    def __init__(self, path):
+       
+        self.types = {'u' : 1, 'l' : 2}
+        
+        self.trainMatrix ,nt= self.load_rating_file_as_matrix(path + "user_artist.dat",path + "user_artist.dat")
+        self.num_users, self.num_items = self.trainMatrix.shape[0], self.trainMatrix.shape[1]
+        self.user_item_map, self.item_user_map, self.train,self.testRatings, self.testNegatives = self.load_rating_file_as_map(path + "user_artist.dat",nt)
+        
+        
+        #self.testRatings, self.testNegatives= self.load_rating_file_as_list(path + "test_tensor.txt",nt)
+        #self.testNegatives = self.load_negative_file(path + "test_negative.txt",self.testRatings)
+        
+        self.user_feature, self.item_feature = self.load_feature_as_map(path+'FM_bpr_train_user_4.txt', path+'FM_bpr_train_item_4.txt')
+        self.fea_size = len(self.user_feature[1])
+        
+        
+        
+        self.path_ulul, self.ulul_path_num, self.ulul_jump = self.load_path_as_map(path + 'fm_train_ulul_limit10_bpr2500_knn_hr_3sim_3_top5.txt')
+        self.path_ulll, self.ulll_path_num, self.ulll_jump = self.load_path_as_map(path + 'fm_train_ulll_limit10_bpr2500_knn_hr_3sim_3_top5.txt')
+        self.path_uuul, self.uuul_path_num, self.uuul_jump = self.load_path_as_map(path + 'fm_train_uuul_limit10_bpr2500_knn_hr_3sim_3_top5.txt')
+        self.path_uull, self.uull_path_num, self.uull_jump = self.load_path_as_map(path + 'fm_train_uull_limit10_bpr2500_knn_hr_3sim_3_top5.txt')
+        '''
+        
+        self.path_ulul, self.ulul_path_num, self.ulul_jump,self.ulul_p = self.load_path_as_map1(path + 'ulul_final_only_positive_addtest.txt')
+        self.path_ulll, self.ulll_path_num, self.ulll_jump,self.ulll_p = self.load_path_as_map1(path + 'ulll_final_only_positive_addtest.txt')
+        self.path_uuul, self.uuul_path_num, self.uuul_jump,self.uuul_p = self.load_path_as_map1(path + 'uuul_final_only_positive_addtest.txt')
+       
+        
+        self.path_ulul, self.ulul_path_num, self.ulul_jump = self.load_path_as_map(path + 'ulul_final_max.txt')
+        self.path_ulll, self.ulll_path_num, self.ulll_jump = self.load_path_as_map(path + 'ulll_final_max.txt')
+        self.path_uuul, self.uuul_path_num, self.uuul_jump = self.load_path_as_map(path + 'uuul_final_max.txt')
+        
+        self.path_ulul1, self.ulul1_path_num, self.ulul1_jump = self.load_path_as_map(path + 'ulul_final22.txt')
+        self.path_ulll1, self.ulll1_path_num, self.ulll1_jump = self.load_path_as_map(path + 'ulll_final22.txt')
+        self.path_uuul1, self.uuul1_path_num, self.uuul1_jump = self.load_path_as_map(path + 'uuul_final22.txt')
+        '''
+    def load_rating_file_as_list(self, filename,trainM):
+        ratingList = []
+        negativeList = []
+        with open(filename, "r") as f:
+            line = f.readline()
+            tmp = np.zeros((self.num_users+1, self.num_items+1))
+            while line != None and line != "":
+                arr = line.split("\t")
+                tmp[int(arr[0])][int(arr[1])] = 1
+                line = f.readline()
+            for i in range (1,self.num_users):
+                tmp_list = []
+                tmp_list1 = []
+                tmp_list.append(i)
+                for j in range(1,self.num_items):
+                    if tmp[i][j] == 1 and trainM[i][j] == 0:
+                        tmp_list.append(j)  
+                    if trainM[i][j] == 0 and tmp[i][j] == 0:
+                        tmp_list1.append(j)
+                ratingList.append(tmp_list)
+                negativeList.append(tmp_list1)
+        return ratingList,negativeList
+    
+    def load_rating_file_as_map(self, filename,trainM):
+        user_item_map = {}
+        item_user_map = {}
+        train = []
+        popularity_dict = {}
+        max_i = 0
+        testRatings = []
+        testNegatives = []
+        for i in range (1,self.num_users):
+            tmp_c = 100
+            tmp_n = []
+            tmp_p = []
+            tmp_p.append(i)
+            count = 0
+            for j in range(1,self.num_items):
+                if trainM[i][j] == 1 and count == 0:
+                    count = 1
+                    tmp_p.append(j)
+                    tmp_n.append(j)
+                elif trainM[i][j] == 1 and count == 1:
+                    train.append([i, j])
+                    max_i = max(max_i, i)
+                    if i not in user_item_map:
+                        user_item_map[i] = {}
+                    if j not in item_user_map:
+                        item_user_map[j] = {}
+                    if j not in popularity_dict:
+                        popularity_dict[j] = 0
+                    user_item_map[i][j] = 1.0
+                    item_user_map[j][i] = 1.0
+                    popularity_dict[j] += 1
+            while tmp_c > 0:
+                tmp_c -= 1
+                k = random.randint(1, self.num_items-1)
+                while trainM[i][k] == 1:
+                    k = random.randint(1, self.num_items-1)
+                tmp_n.append(k)
+            testNegatives.append(tmp_n)
+            testRatings.append(tmp_p)
+        item_popularity = [0] * max_i
+        
+        return user_item_map, item_user_map, train,testRatings,testNegatives
+
+    def load_rating_file_as_matrix(self, filename, filename1):
+      
+        num_users, num_items = 0, 0
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line != None and line != "":
+                arr = line.split("\t")
+                u, i = int(arr[0]), int(arr[1])
+                num_users = max(num_users, u)
+                num_items = max(num_items, i)
+                line = f.readline()
+        # Construct matrix
+        mat = sp.dok_matrix((num_users+1, num_items+1), dtype=np.float32)
+        nt = np.zeros((num_users+1, num_items+1))
+        train_list = []
+        with open(filename, "r") as f:
+            line = f.readline()
+            while line != None and line != "":
+                arr = line.split("\t")
+                user, item = int(arr[0]), int(arr[1])
+                train_list.append([user, item])
+                mat[user, item] = 1.0
+                nt[user][item] = 1
+                line = f.readline()    
+                
+        with open(filename1, "r") as f:
+            line = f.readline()
+            while line != None and line != "":
+                arr = line.split("\t")
+                user, item = int(arr[0]), int(arr[1])
+                train_list.append([user, item])
+                mat[user, item] = 1.0
+                nt[user][item] = 1
+                line = f.readline()    
+                
+        return mat ,nt
+
+    def load_feature_as_map(self, user_fea_file, item_fea_file):
+        user_feature = np.zeros((self.num_users, 8))
+        item_feature = np.zeros((self.num_items, 8))
+        
+        with open(user_fea_file) as infile:
+            t = 1
+            for line in infile.readlines():
+                arr = line.split('\t')
+                if t <= 2321:
+                    for j in range(len(arr)-1):
+                        user_feature[t][j] = float(arr[j])
+                    t += 1    
+            
+            
+        with open(item_fea_file) as infile:
+            t = 1
+            for line in infile.readlines():
+                arr = line.split('\t')
+                #item_feature[i] = list()
+                
+                for j in range(len(arr)-1):
+                    item_feature[t][j] = float(arr[j])
+                t += 1    
+       
+        return user_feature, item_feature
+        
+    def load_path_as_map1(self, filename):
+        print (filename) 
+        path_dict = {}
+        path_dict_num = {}
+        path_num = 5
+        jump = 4
+        length = 2
+        ctn = 0
+        with open(filename) as infile:
+            line = infile.readline()
+            while line != None and line != "":
+                line = infile.readline()
+                ctn += 1
+        print (ctn, path_num, jump, length)
+        with open(filename) as infile:
+            line = infile.readline()
+
+            while line != None and line != "":
+                u_index = 0
+                l_index = 0
+                tmp_list1 = line.split(',')
+                ul_list = tmp_list1[0].split('\t')
+                u_index = int(ul_list[0])
+                l_index = int(ul_list[1])
+                tmp_list = tmp_list1[1].split('\t')
+                path_dict[(u_index, l_index)] = []
+                path_dict_num[(u_index, l_index)] = []
+                path_dict_num[(u_index, l_index)].append(len(tmp_list)-1)
+                for tt in range(0, len(tmp_list)-1):
+                    tmp = tmp_list[tt].split('-')
+                    node_list = []
+                    t = 0
+                    for node in tmp:
+                        
+                        index = int(node[1:])
+                        node_list.append([self.types[node[0]], index])
+                        t += 1
+                    path_dict[(u_index, l_index)].append(node_list)
+                line = infile.readline()        
+        return path_dict, path_num, jump,path_dict_num
+    
+    def load_path_as_map(self, filename):
+        print (filename) 
+        path_dict = {}
+        path_dict_num = {}
+        path_num = 5
+        jump = 4
+        length = 2
+        ctn = 0
+        with open(filename) as infile:
+            line = infile.readline()
+            while line != None and line != "":
+                line = infile.readline()
+                ctn += 1
+        print (ctn, path_num, jump, length)
+        with open(filename) as infile:
+            line = infile.readline()
+            while line != None and line != "":
+                
+                u_index = 0
+                l_index = 0
+               
+                tmp_list = line.split('\t')
+                for tt in range(0, len(tmp_list)-1):
+                    tmp = tmp_list[tt].split('-')
+                    node = tmp[0]
+                    u_index = int(node[1:])
+                    node = tmp[3]
+                    l_index = int(node[1:])
+                path_dict[(u_index, l_index)] = []
+                path_dict_num[(u_index, l_index)] = []
+                #path_dict_num[(u_index, l_index)].append(len(tmp_list)-1)
+                for tt in range(0, len(tmp_list)-1):
+                    tmp = tmp_list[tt].split('-')
+                    node_list = []
+                    t = 0
+                    for node in tmp:
+                        index = int(node[1:])
+                        node_list.append([self.types[node[0]], index])
+                        t += 1
+                    path_dict[(u_index, l_index)].append(node_list)
+                line = infile.readline()       
+        return path_dict, path_num, jump
+	
+
+if __name__ == '__main__':
+    dataset = Dataset('tmp/')
+    
+
---
+++ evaluate_hr_4type.py
@@ -0,0 +1,266 @@
+'''
+Created on Apr 15, 2016
+Evaluate the performance of Top-K recommendation:
+    Protocol: leave-1-out evaluation
+    Measures: Hit Ratio and NDCG
+    (more details are in: Xiangnan He, et al. Fast Matrix Factorization for Online Recommendation with Implicit Feedback. SIGIR'16)
+
+@author: hexiangnan
+'''
+import math
+import heapq # for retrieval topK
+import multiprocessing
+import numpy as np
+from time import time
+import scipy.sparse as sp
+import gc
+#from numba import jit, autojit
+
+# Global variables that are shared across processes
+_model = None
+_testRatings = None
+_testNegatives = None
+_K = None
+_num_users = None
+_num_items = None
+_path_ulul = None
+_path_ulll = None
+_path_uuul = None
+_path_uull = None
+_path_nums = None
+_jumps = None
+_length = None
+
+_user_feature = None
+_item_feature = None
+_type_feature = None
+_features = None
+def evaluate_model(model, user_feature, item_feature, num_users, num_items, path_ulul, path_ulll, path_uuul, path_uull, path_nums, jumps, length, testRatings, testNegatives, K, num_thread):
+    """
+    Evaluate the performance (Hit_Ratio, NDCG) of top-K recommendation
+    Return: score of each test rating.
+    """
+    global _model
+    global _testRatings
+    global _testNegatives
+    global _K
+    global _num_users
+    global _num_items
+    global _path_ulul
+    global _path_ulll
+    global _path_uuul
+    global _path_uull
+    global _path_nums
+    global _jumps
+    global _length
+
+    global _user_feature
+    global _item_feature
+    global _features
+
+    _model = model
+    _testRatings = testRatings
+    _testNegatives = testNegatives
+    _K = K
+    _num_users = num_users
+    _num_items = num_items
+    _path_ulul = path_ulul
+    _path_ulll = path_ulll
+    _path_uuul = path_uuul
+    _path_uull = path_uull
+    _path_nums = path_nums
+    _jumps = jumps
+    _length = length
+
+    _user_feature = user_feature
+    _item_feature = item_feature
+    _features = [user_feature, item_feature]
+    ps, ndcgs, ps1, ndcgs1, ps2, ndcgs2 = [], [], [] , [], [], []
+    if(num_thread > 1): # Multi-thread
+        pool = multiprocessing.Pool(processes=num_thread)
+        res = pool.map(eval_one_rating, range(len(_testRatings)))
+        pool.close()
+        pool.join()
+        hits = [r[0] for r in res]
+        ndcgs = [r[1] for r in res]
+        return (hits, ndcgs)
+    # Single thread
+    #eval_one_rating1()
+    
+    for idx in range(len(_testRatings)):
+        if idx % 1 == 0:
+            (p, ndcg, p1, ndcg1, p2 ,ndcg2) = eval_one_rating(idx)
+            ps.append(p)
+            ndcgs.append(ndcg)    
+            ps1.append(p1)
+            ndcgs1.append(ndcg1)    
+            ps2.append(p2)
+            ndcgs2.append(ndcg2)    
+    '''
+        
+    if np.array(ps).mean() > 0.4:
+        print('wrong predication')
+        for idx in range(len(_testRatings)):
+            if idx < 10:
+                (p, r, ndcg) = eval_one_rating1(idx)
+               
+    
+    ps1 = np.array(ps).mean()
+    if ps1 > 0.3:
+        print('wrong prediction:')
+        for idx in range(len(_testRatings)):
+             p = eval_one_rating1(idx)
+        return 0
+    '''    
+    return (ps, ndcgs, ps1, ndcgs1, ps2, ndcgs2)
+
+
+
+def eval_one_rating(idx):
+    rating = _testRatings[idx]
+    items = _testNegatives[idx]
+    u = rating[0]
+    gtItems = rating[1:]
+    
+    #items += gtItems
+    #items.append(gtItem)
+    
+    # Get prediction scores
+    map_item_score = {}
+    user_input = []
+    item_input = []
+    user_fe = np.zeros((len(items),  8))
+    item_fe = np.zeros((len(items),  8))
+    ulul_input = np.zeros((len(items), _path_nums[0], _jumps[0], _length))
+    ulll_input = np.zeros((len(items), _path_nums[1], _jumps[1], _length))
+    uuul_input = np.zeros((len(items), _path_nums[2], _jumps[2], _length)) 
+    uull_input = np.zeros((len(items), _path_nums[3], _jumps[3], _length)) 
+    k = 0
+ 
+    for i in items:
+       
+        user_input.append(u)
+        item_input.append(i)
+        user_fe[k] = _user_feature[u]
+        item_fe[k] = _item_feature[i]
+        if (u, i) in _path_ulul: 
+            for p_i in range(len(_path_ulul[(u, i)])):
+                for p_j in range(len(_path_ulul[(u, i)][p_i])):
+                    type_id = _path_ulul[(u, i)][p_i][p_j][0]
+                    index = _path_ulul[(u, i)][p_i][p_j][1]
+                    if type_id == 1:
+                        ulul_input[k][p_i][p_j] = _user_feature[index]
+                    elif type_id == 2:
+                        ulul_input[k][p_i][p_j] = _item_feature[index]
+            
+        if (u, i) in _path_ulll:
+            for p_i in range(len(_path_ulll[(u, i)])):
+                for p_j in range(len(_path_ulll[(u, i)][p_i])):
+                    type_id = _path_ulll[(u, i)][p_i][p_j][0]
+                    index = _path_ulll[(u, i)][p_i][p_j][1]
+                    if type_id == 1:
+                        ulll_input[k][p_i][p_j] = _user_feature[index]
+                    elif type_id == 2:
+                        ulll_input[k][p_i][p_j] = _item_feature[index]
+                   
+        if (u, i) in _path_uuul:
+            for p_i in range(len(_path_uuul[(u, i)])):
+                for p_j in range(len(_path_uuul[(u, i)][p_i])):
+                    type_id = _path_uuul[(u, i)][p_i][p_j][0]
+                    index = _path_uuul[(u, i)][p_i][p_j][1]
+                    if type_id == 1:
+                        uuul_input[k][p_i][p_j] = _user_feature[index]
+                    elif type_id == 2:
+                        uuul_input[k][p_i][p_j] = _item_feature[index]
+                        
+        if (u, i) in _path_uull:
+            for p_i in range(len(_path_uull[(u, i)])):
+                for p_j in range(len(_path_uull[(u, i)][p_i])):
+                    type_id = _path_uull[(u, i)][p_i][p_j][0]
+                    index = _path_uull[(u, i)][p_i][p_j][1]
+                    if type_id == 1:
+                        uuul_input[k][p_i][p_j] = _user_feature[index]
+                    elif type_id == 2:
+                        uuul_input[k][p_i][p_j] = _item_feature[index]
+        k += 1
+
+    
+    predictions = _model.predict([np.array(user_input), np.array(item_input), ulul_input, ulll_input, uuul_input, uull_input], 
+                                 batch_size = 256, verbose = 0)
+    
+    
+    for i in range(len(items)):
+        item = items[i]
+        map_item_score[item] = predictions[i] 
+    '''
+    ii = 0
+    for i in items:
+        map_item_score[i] = predictions[ii]
+        ii += 1
+    '''
+    #items.pop()
+    # Evaluate top rank list
+    
+    ranklist = heapq.nlargest(10, map_item_score, key=map_item_score.get)
+    p = getP(ranklist[:3], gtItems,items)
+    ndcg = getNDCG(ranklist[:3], gtItems)
+    p1 = getP(ranklist[:5], gtItems,items)
+    ndcg1 = getNDCG(ranklist[:5], gtItems)
+    p2 = getP(ranklist, gtItems,items)   
+    ndcg2 = getNDCG(ranklist[:10], gtItems)
+    return (p, ndcg, p1, ndcg1, p2 ,ndcg2)
+
+
+
+def getP(ranklist, gtItems,items):
+    p = 0
+    for item in ranklist:
+        if item in gtItems:
+            p += 1
+      
+    return p * 1.0
+
+def getR(ranklist, gtItems):
+    r = 0
+    for item in ranklist:
+        if item in gtItems:
+            r += 1
+    return r * 1.0 / len(gtItems)
+
+def getHitRatio(ranklist, gtItem):
+    for item in ranklist:
+        if item == gtItem:
+            return 1
+    return 0
+
+def getDCG(ranklist, gtItems):
+    dcg = 0.0
+    for i in range(len(ranklist)):
+        item = ranklist[i]
+        if item in gtItems:
+            dcg += 1.0 / math.log(i + 2)
+    return  dcg
+
+def getIDCG(ranklist, gtItems):
+    idcg = 0.0
+    i = 0
+    for item in ranklist:
+        if item in gtItems:
+            idcg += 1.0 / math.log(i + 2)
+            i += 1
+    return idcg 
+
+def getNDCG(ranklist, gtItems):
+    dcg = getDCG(ranklist, gtItems)
+    idcg = getIDCG(ranklist, gtItems)
+    if idcg == 0:
+        return 0
+    return dcg / idcg
+
+
+#def getNDCG(ranklist, gtItems):
+#    for i in xrange(len(ranklist)):
+#        item = ranklist[i]
+#        if item == gtItems:
+#            return math.log(2) / math.log(i+2)
+#    return 0
